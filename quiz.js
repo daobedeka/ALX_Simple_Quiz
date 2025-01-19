1e1a0d6a2a67709760bbf4b51725636905fa6f9c
Daniel Obedeka <daobedeka@gmail.com>
2025-01-19 23:29:17 +0000

    completed project1

---
+++ quiz.js
@@ -0,0 +1,19 @@
+function checkAnswer() {
+    // Function body
+    const correctAnswer='4'
+    
+ const userAnswer=document.querySelector("input[type=radio][name=quiz]:checked").value
+ let feedback=document.getElementById('feedback')
+ if(userAnswer==correctAnswer){
+    feedback.textContent="Correct! Well done."
+
+ }
+ else{
+    feedback.textContent="That's incorrect. Try again!"
+ }
+
+}
+
+const submit=document.getElementById('submit-answer');
+submit.addEventListener("click",checkAnswer)
+
